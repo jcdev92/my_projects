6c656f261382d81d181a046887e8102ab6e41c09
jcdev92 <jcdeveloper92@gmail.com>
2022-11-29 20:20:40 -0400

    horizontal styles, side bar profile, and some functions added

--- 04-navigatonApp/source/navigator/DrawerNavigator.tsx
+++ 04-navigatonApp/source/navigator/DrawerNavigator.tsx
@@ -1,16 +1,50 @@
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import {createDrawerNavigator, DrawerContentComponentProps, DrawerContentScrollView} from '@react-navigation/drawer';
 import {StackNavigator} from "./StackNavigator";
 import {SettingsScreen} from "../screens/SettingsScreen";
-import {useWindowDimensions} from "react-native";
+import {Image, Text, useWindowDimensions, View} from "react-native";
 
 const Drawer = createDrawerNavigator();
 
 export const DrawerNavigator = () => {
-    const {} = useWindowDimensions()
+    const {width} = useWindowDimensions()
+    const isLargeScreen = width >= 768;
+
     return (
-        <Drawer.Navigator>
-            <Drawer.Screen name="StackNavigator" component={StackNavigator} />
-            <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
+        <Drawer.Navigator
+            screenOptions={
+                {
+                    drawerType: isLargeScreen ? 'permanent' : 'front',
+                }
+            }
+            drawerContent={(props) => <SideMenu {...props} />}
+        >
+            <Drawer.Screen name="StackNavigator" component={StackNavigator}/>
+            <Drawer.Screen name="SettingsScreen" component={SettingsScreen}/>
         </Drawer.Navigator>
     );
+}
+
+
+const SideMenu = (props: DrawerContentComponentProps) => {
+    return (
+        <DrawerContentScrollView>
+            <View>
+                <Image source={
+                    {
+                        uri: 'https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png'
+                    }
+                }
+                       style={{
+                           width: 80,
+                           height: 80,
+                           borderRadius: 40,
+                           borderWidth: 2,
+                           borderColor: 'white',
+                           marginTop: 20,
+                           marginLeft: 20,
+                       }}
+                />
+            </View>
+        </DrawerContentScrollView>
+    )
 }
